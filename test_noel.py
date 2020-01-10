970313bbc1d799a83c8f147b0ed461dcb0c4eae4
Silvério Mantovaneli <silveriomm@bol.com.br>
2020-01-09 23:44:10 -0300

    Editing test_noel.py

---
+++ test_noel.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+
+"""
+@autor: Silvério Mantovaneli
+
+"""
+
+import unittest
+from noel import solution
+
+def test_one_Ho(self):
+	self.assertEqual(solution(1), "Ho")
+
+unittest.main()
