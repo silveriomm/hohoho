869937eb833e1fc71860020a56f929cc13bc0ae8
Silvério Mantovaneli <silveriomm@bol.com.br>
2020-01-10 08:07:38 -0300

    terceiro teste, falhou

--- test_noel.py
+++ test_noel.py
@@ -16,5 +16,8 @@ class TestNoel(unittest.TestCase):
 	def test_two_Ho(self):
 		self.assertEqual(solution(2), "Ho Ho!")
 
+	def test_five_Ho(self):
+		self.assertEqual(solution(2), "Ho Ho Ho Ho Ho!")
+
 
 unittest.main()
